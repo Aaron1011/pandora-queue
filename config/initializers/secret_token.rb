24d6c2f3734ebae16554debda80f177facfed3a4
Aaron Hill <aa1ronham@aol.com>
2013-02-09 21:29:53 -0500

    Created initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PandoraQueue::Application.config.secret_token = 'cd6d3a79ae0be075a69fbf7bca0f5a0cb8f9feda13efd93ed3ca0e16c0542c1d3549d5d36175556ca71ab1c901734d006715ed394c344a65485fce35849e02d5'
